009e433e56dc5bede8d6799d5606b41e56cbb912
Jacques-z <89645196+Jacques-z@users.noreply.github.com>
2026-04-09 19:30:06 +0800

    Add llama.cpp dependencies

--- flake.nix
+++ flake.nix
@@ -15,6 +15,10 @@
           pkg-config
           gobject-introspection
           cargo
+          clang
+          cmake
+          llvmPackages.libclang
+          ninja
           nodejs
           pnpm
           typescript
@@ -56,8 +60,10 @@
             export XDG_DATA_DIRS=${pkgs.gsettings-desktop-schemas}/share/gsettings-schemas/${pkgs.gsettings-desktop-schemas.name}:${pkgs.gtk3}/share/gsettings-schemas/${pkgs.gtk3.name}:$XDG_DATA_DIRS
             # for TLS:
             export GIO_MODULE_DIR=${pkgs.glib-networking}/lib/gio/modules/
+            export LIBCLANG_PATH=${pkgs.llvmPackages.libclang.lib}/lib
             export LD_LIBRARY_PATH=${pkgs.lib.makeLibraryPath [
               pkgs.libayatana-appindicator
+              pkgs.llvmPackages.libclang.lib
               pkgs.libappindicator-gtk3  # optional fallback name the crate also probes
             ]}:$LD_LIBRARY_PATH
           '';
